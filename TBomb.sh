a1e126eda9635483972eb1026af296b3889d39f4
skp121 <58383473+skp121@users.noreply.github.com>
2020-01-01 11:23:19 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -1,5 +1,112 @@
-import zlib, base64
-
-encodedStr = "IyEvYmluL2Jhc2gKY2xlYXIKZWNobyAtZSAiXGVbNDszMW0gU0tQIFByb2R1Y3Rpb25zICEhISBcZVswbSIKZWNobyAtZSAiXGVbMTszNG0gUHJlc2VudHMgXGVbMG0iCmVjaG8gLWUgIlxlWzE7MzJtIFRCT01CLUVESVRFRCBcZVswbSIKZWNobyAiUHJlc3MgRW50ZXIgVG8gQ29udGludWUiCnJlYWQgYTEKaWYgW1sgLXMgdXBkYXRlLlNLUCBdXTt0aGVuCmVjaG8gIkFsbCBSZXF1aXJlbWVudHMgRm91bmQuLi4uIgplbHNlCmVjaG8gJ0luc3RhbGxpbmcgUmVxdWlyZW1lbnRzLi4uLicKZWNobyAuCmVjaG8gLgphcHQgaW5zdGFsbCBmaWdsZXQgdG9pbGV0IHB5dGhvbiBjdXJsIC15CmFwdCBpbnN0YWxsIHB5dGhvbjMtcGlwCnBpcCBpbnN0YWxsIC1yIHJlcXVpcmVtZW50cy50eHQKZWNobyBUaGlzIFNjcmlwdCBXYXMgTWFkZSBCeSBTS1AgPnVwZGF0ZS5zcGVlZHgKZWNobyBSZXF1aXJlbWVudHMgSW5zdGFsbGVkLi4uLgplY2hvIFByZXNzIEVudGVyIFRvIENvbnRpbnVlLi4uCnJlYWQgdXBkCmZpCndoaWxlIDoKZG8Kcm0gKi54eHggPi9kZXYvbnVsbCAyPiYxCmNsZWFyCmVjaG8gLWUgIlxlWzE7MzFtIgpmaWdsZXQgVEJPTUItRURJVEVECmVjaG8gLWUgIlxlWzE7MzRtIENyZWF0ZWQgQnkgXGVbMTszMm0iCnRvaWxldCAtZiBtb25vMTIgLUYgYm9yZGVyIFNLUAplY2hvIC1lICJcZVs0OzM0bSBUaGlzIEJvbWJlciBXYXMgQ3JlYXRlZCBCeSBTS1AgXGVbMG0iCmVjaG8gIiAiCmVjaG8gLWUgIlxlWzQ7MzFtIFBsZWFzZSBSZWFkIEluc3RydWN0aW9uIENhcmVmdWxseSAhISEgXGVbMG0iCmVjaG8gIiAiCmVjaG8gIlByZXNzIDEgVG8gIFN0YXJ0IFNNUyBCb21iZXIgIgplY2hvICJQcmVzcyAyIFRvICBTdGFydCBDYWxsIEJvbWJlciAiCmVjaG8gIlByZXNzIDMgVG8gIFVwZGF0ZSAoV29ya3MgT24gTGludXggQW5kIExpbnV4IEVtdWxhdG9ycykgIgplY2hvICJQcmVzcyA0IFRvICBWaWV3IEZlYXR1cmVzICIKZWNobyAiUHJlc3MgNSBUbyAgRXhpdCAiCnJlYWQgY2gKaWYgWyAkY2ggLWVxIDEgXTt0aGVuCmNsZWFyCmVjaG8gLWUgIlxlWzE7MzJtIgpybSAqLnh4eCA+L2Rldi9udWxsIDI+JjEKcHl0aG9uMyBib21iZXIucHkKcm0gKi54eHggPi9kZXYvbnVsbCAyPiYxCmV4aXQgMAplbGlmIFsgJGNoIC1lcSAyIF07dGhlbgpjbGVhcgplY2hvIC1lICJcZVsxOzMybSIKZWNobyAnQ2FsbCBCb21iIEJ5IFNLUCc+IGNhbGwueHh4CnB5dGhvbjMgYm9tYmVyLnB5IGNhbGwKcm0gKi54eHggPi9kZXYvbnVsbCAyPiYxCmV4aXQgMAplbGlmIFsgJGNoIC1lcSAzIF07dGhlbgpjbGVhcgphcHQgaW5zdGFsbCBnaXQgLXkKZWNobyAtZSAiXGVbMTszNG0gRG93bmxvYWRpbmcgTGF0ZXN0IEZpbGVzLi4uIgpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL3NrcDEyMS9UQk9NQi1FRElURUQKaWYgW1sgLXMgVEJvbWIvVEJvbWIuc2ggXV07dGhlbgpjZCBUQk9NQi1FRElURUQKY3AgLXIgLWYgKiAuLiA+IHRlbXAKY2QgLi4Kcm0gLXJmICBUQm9tYiA+PiB0ZW1wCnJtIHVwZGF0ZS5zcGVlZHggPj4gdGVtcApybSB0ZW1wCmNobW9kICt4IFRCb21iLnNoCmZpCmVjaG8gLWUgIlxlWzE7MzJtIFRCb21iIFdpbGwgUmVzdGFydCBOb3cuLi4iCmVjaG8gLWUgIlxlWzE7MzJtIEFsbCBUaGUgUmVxdWlyZWQgUGFja2FnZXMgV2lsbCBCZSBJbnN0YWxsZWQuLi4iCmVjaG8gLWUgIlxlWzE7MzRtIFByZXNzIEVudGVyIFRvIFByb2NlZWQgVG8gUmVzdGFydC4uLiIKcmVhZCBhNgouL1RCb21iLnNoCmV4aXQKZWxpZiBbICRjaCAtZXEgNCBdO3RoZW4KY2xlYXIKZWNobyAtZSAiXGVbMTszM20iCmZpZ2xldCBUQk9NQi1FRElURUQKZWNobyAtZSAiXGVbMTszNG1DcmVhdGVkIEJ5IFxlWzE7MzRtIgp0b2lsZXQgLWYgbW9ubzEyIC1GIGJvcmRlciBTS1AKZWNobyAgIiAiCmVjaG8gLWUgIlxlWzE7MzJtICAgICAgICAgICAgICAgICAgIEZlYXR1cmVzXGVbMTszNG0iCmVjaG8gIiAgWytdIFVubGltaXRlZCBBbmQgU3VwZXItRmFzdCBCb21iaW5nIgplY2hvICIgIFsrXSBJbnRlcm5hdGlvbmFsIEJvbWJpbmciCmVjaG8gIiAgWytdIENhbGwgQm9tYmluZyAiCmVjaG8gIiAgWytdIFByb3RlY3Rpb24gTGlzdCIKZWNobyAiICBbK10gQXV0b21hdGVkIEZ1dHVyZSBVcGRhdGVzIgplY2hvICIgIFsrXSBFYXN5IFRvIFVzZSBBbmQgRW1iZWQgaW4gQ29kZSIKZWNobyAiIgplY2hvICIiCmVjaG8gLWUgIlxlWzE7MzFtIFRoaXMgU2NyaXB0IGlzIE9ubHkgRm9yIEVkdWNhdGlvbmFsIFB1cnBvc2VzIG9yIFRvIFByYW5rLlxlWzBtIgplY2hvIC1lICJcZVsxOzMxbSBEbyBub3QgVXNlIFRoaXMgVG8gSGFybSBPdGhlcnMuIFxlWzBtIgplY2hvIC1lICJcZVsxOzMxbSBJIEFtIE5vdCBSZXNwb25zaWJsZSBGb3IgVGhlIE1pc3VzZSBPZiBUaGUgU2NyaXB0LiBcZVswbSIKZWNobyAtZSAiXGVbMTszMm0gTWFrZSBTdXJlIFRvIFVwZGF0ZSBpdCBJZiBJdCBEb2VzIG5vdCBXb3JrLlxlWzBtIgplY2hvICAiICIKZWNobyAiUHJlc3MgRW50ZXIgVG8gR28gSG9tZSIKcmVhZCBhMwpjbGVhcgplbGlmIFsgJGNoIC1lcSA1IF07dGhlbgpjbGVhcgplY2hvIC1lICJcZVsxOzMxbSIKZmlnbGV0IFRCT01CLUVESVRFRAplY2hvIC1lICJcZVsxOzM0bSBDcmVhdGVkIEJ5IFxlWzE7MzJtIgp0b2lsZXQgLWYgbW9ubzEyIC1GIGJvcmRlciBTS1AKZWNobyAiICIKZXhpdCAwCmVsc2UKZWNobyAtZSAiXGVbNDszMm0gSW52YWxpZCBJbnB1dCAhISEgXGVbMG0iCmVjaG8gIlByZXNzIEVudGVyIFRvIEdvIEhvbWUiCnJlYWQgYTMKY2xlYXIKZmkKZG9uZQ=="
-
-exec(base64.b64decode(encodedStr))
+#!/bin/bash
+clear
+echo -e "\e[4;31m SKP Productions !!! \e[0m"
+echo -e "\e[1;34m Presents \e[0m"
+echo -e "\e[1;32m TBOMB-EDITED \e[0m"
+echo "Press Enter To Continue"
+read a1
+if [[ -s update.SKP ]];then
+echo "All Requirements Found...."
+else
+echo 'Installing Requirements....'
+echo .
+echo .
+apt install figlet toilet python curl -y
+apt install python3-pip
+pip install -r requirements.txt
+echo This Script Was Made By SKP >update.speedx
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+figlet TBOMB-EDITED
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border SKP
+echo -e "\e[4;34m This Bomber Was Created By SKP \e[0m"
+echo " "
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+echo " "
+echo "Press 1 To  Start SMS Bomber "
+echo "Press 2 To  Start Call Bomber "
+echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
+echo "Press 4 To  View Features "
+echo "Press 5 To  Exit "
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 bomber.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By SKP'> call.xxx
+python3 bomber.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..."
+git clone https://github.com/skp121/TBOMB-EDITED
+if [[ -s TBomb/TBomb.sh ]];then
+cd TBOMB-EDITED
+cp -r -f * .. > temp
+cd ..
+rm -rf  TBomb >> temp
+rm update.speedx >> temp
+rm temp
+chmod +x TBomb.sh
+fi
+echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."
+read a6
+./TBomb.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"
+figlet TBOMB-EDITED
+echo -e "\e[1;34mCreated By \e[1;34m"
+toilet -f mono12 -F border SKP
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m"
+echo "  [+] Unlimited And Super-Fast Bombing"
+echo "  [+] International Bombing"
+echo "  [+] Call Bombing "
+echo "  [+] Protection List"
+echo "  [+] Automated Future Updates"
+echo "  [+] Easy To Use And Embed in Code"
+echo ""
+echo ""
+echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
+echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
+echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
+echo  " "
+echo "Press Enter To Go Home"
+read a3
+clear
+elif [ $ch -eq 5 ];then
+clear
+echo -e "\e[1;31m"
+figlet TBOMB-EDITED
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border SKP
+echo " "
+exit 0
+else
+echo -e "\e[4;32m Invalid Input !!! \e[0m"
+echo "Press Enter To Go Home"
+read a3
+clear
+fi
+done

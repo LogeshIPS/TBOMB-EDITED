853cf6c51104e2bde54d4772e880ff2f1a39b046
skp121 <58383473+skp121@users.noreply.github.com>
2020-02-29 05:25:58 +0000

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SKP Productions !!! \e[0m"
+echo -e "\e[4;31m Dark Angel Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m TBOMB-EDITED \e[0m"
 echo "Press Enter To Continue"
 read a1
-if [[ -s update.SKP ]];then
+if [[ -s update.Dark Angel ]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SKP >update.speedx
+echo This Script Was Made By Dark Angel >update.Dark Angel
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -26,16 +26,16 @@ clear
 echo -e "\e[1;31m"
 figlet TBOMB-EDITED
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SKP
-echo -e "\e[4;34m This Bomber Was Created By SKP \e[0m"
+toilet -f mono12 -F border Dark Angel
+echo -e "\e[4;34m This Bomber Was Created By Dark Angel \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
-echo "Press 1 To  Start SMS Bomber "
-echo "Press 2 To  Start Call Bomber "
-echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
-echo "Press 4 To  View Features "
-echo "Press 5 To  Exit "
+echo "Click 1 To  Start SMS Bomber "
+echo "Click 2 To  Start Call Bomber "
+echo "Click 3 To  Update (Works On Linux And Linux Emulators) "
+echo "Click 4 To  View Features "
+echo "Click 5 To  Exit "
 read ch
 if [ $ch -eq 1 ];then
 clear
@@ -47,7 +47,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SKP'> call.xxx
+echo 'Call Bomb By Dark Angel'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -55,17 +55,17 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/skp121/TBOMB-EDITED
-if [[ -s TBomb/TBomb.sh ]];then
+git clone https://github.com/Dark Angel/TBOMB-EDITED
+if [[ -s TBOMB-EDITED/TBomb.sh ]];then
 cd TBOMB-EDITED
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
+rm -rf  TBOMB-EDITED >> temp
+rm update.Dark Angel >> temp
 rm temp
 chmod +x TBomb.sh
 fi
-echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m TBOMB-EDITED Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
@@ -74,9 +74,9 @@ exit
 elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;33m"
-figlet TBOMB-EDITED
+figlet TB0MB-EDITED
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SKP
+toilet -f mono12 -F border Dark Anel
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -92,6 +92,7 @@ echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
 echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
 echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
+echo -e "\e[4;31m That's All !!!\e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
@@ -100,7 +101,7 @@ clear
 echo -e "\e[1;31m"
 figlet TBOMB-EDITED
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SKP
+toilet -f mono12 -F border Dark Angel
 echo " "
 exit 0
 else
